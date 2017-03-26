0d5760f32dfad58f23224f82fb586afce1978b79
Alex Benoit <alexander.benoit@outlook.com>
2017-03-26 20:51:02 +0200

    fixed images and favicons and added feature icons on multi page scroll setup@

--- js/application.js
+++ js/application.js
@@ -1,71 +1,87 @@
 // NAVBAR DESIGN
-$(document).ready(function(){
-  var userHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  var startFade = userHeight - userHeight/2;
-  var endFade = userHeight - 70;
-  var $navbar = $('#navbar');
-  var st = $(this).scrollTop();
-  if ( st <= startFade ) {
-    $navbar.css({ 'background' : 'rgba(28, 84, 156, 0.0)' });
-  } else if ( st > startFade && st < endFade ) {
-    var transparancy = ((st/endFade - startFade/endFade) / (1 - startFade/endFade));
-    $navbar.css({ 'background' : `rgba(28, 84, 156, ${transparancy})` });
-  } else {
-    $navbar.css({ 'background' : 'rgba(28, 84, 156, 1)' });
-  }
-
-  $(window).on('scroll', function() {
-    var st = $(this).scrollTop();
-    if ( st <= startFade ) {
-      $navbar.css({ 'background' : 'rgba(28, 84, 156, 0.0)' });
-    } else if ( st > startFade && st < endFade ) {
-      var transparancy = ((st/endFade - startFade/endFade) / (1 - startFade/endFade));
-      $navbar.css({ 'background' : `rgba(28, 84, 156, ${transparancy})` });
+$(function(){
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > $(window).height() - 70) {
+      $('#navbar:hidden').slideDown();
     } else {
-      $navbar.css({ 'background' : 'rgba(28, 84, 156, 1)' });
+      $('#navbar:visible').slideUp();
     }
   });
+
+  if ($(this).scrollTop() > $(window).height() - 70) {
+    $('#navbar:hidden').slideDown();
+  } else {
+    $('#navbar:visible').slideUp();
+  }
 });
+
+// $(function(){
+//   var userHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+//   var startFade = userHeight - userHeight/2;
+//   var endFade = userHeight - 70;
+//   var $navbar = $('#navbar');
+//   var st = $(this).scrollTop();
+//   if ( st <= startFade ) {
+//     $navbar.css({ 'background' : 'rgba(28, 84, 156, 0.0)' });
+//   } else if ( st > startFade && st < endFade ) {
+//     var transparancy = ((st/endFade - startFade/endFade) / (1 - startFade/endFade));
+//     $navbar.css({ 'background' : `rgba(28, 84, 156, ${transparancy})` });
+//   } else {
+//     $navbar.css({ 'background' : 'rgba(28, 84, 156, 1)' });
+//   }
+
+//   $(window).on('scroll', function() {
+//     var st = $(this).scrollTop();
+//     if ( st <= startFade ) {
+//       $navbar.css({ 'background' : 'rgba(28, 84, 156, 0.0)' });
+//     } else if ( st > startFade && st < endFade ) {
+//       var transparancy = ((st/endFade - startFade/endFade) / (1 - startFade/endFade));
+//       $navbar.css({ 'background' : `rgba(28, 84, 156, ${transparancy})` });
+//     } else {
+//       $navbar.css({ 'background' : 'rgba(28, 84, 156, 1)' });
+//     }
+//   });
+// });
 
 // MENU LINKS TO SECTIONS
-$(document).ready(function(){
-  $('#navbar').on('click', '#toAbout', function() {
-    event.preventDefault();
-    var targetOffset = $('#about').offset().top - 70;
-    $("html, body").animate({scrollTop: targetOffset}, 500);
-  });
+// $(function(){
+//   $('#navbar').on('click', '#toAbout', function() {
+//     event.preventDefault();
+//     var targetOffset = $('#about').offset().top - 70;
+//     $("html, body").animate({scrollTop: targetOffset}, 500);
+//   });
 
-  // TODO
-  $('#navbar').on('click', '#toResume', function() {
-    event.preventDefault();
-    var targetOffset = $('#resume').offset().top - 70;
-    $("html, body").animate({scrollTop: targetOffset}, 500);
-  });
+//   // TODO
+//   $('#navbar').on('click', '#toResume', function() {
+//     event.preventDefault();
+//     var targetOffset = $('#resume').offset().top - 70;
+//     $("html, body").animate({scrollTop: targetOffset}, 500);
+//   });
 
-  $('#navbar').on('click', '#toWork', function() {
-    event.preventDefault();
-    var targetOffset = $('#work').offset().top - 70;
-    $("html, body").animate({scrollTop: targetOffset}, 500);
-  });
+//   $('#navbar').on('click', '#toWork', function() {
+//     event.preventDefault();
+//     var targetOffset = $('#work').offset().top - 70;
+//     $("html, body").animate({scrollTop: targetOffset}, 500);
+//   });
 
-  $('#navbar').on('click', '#toContact', function() {
-    event.preventDefault();
-    var targetOffset = $('#contact').offset().top - 70;
-    $("html, body").animate({scrollTop: targetOffset}, 500);
-  });
+//   $('#navbar').on('click', '#toContact', function() {
+//     event.preventDefault();
+//     var targetOffset = $('#contact').offset().top - 70;
+//     $("html, body").animate({scrollTop: targetOffset}, 500);
+//   });
 
-  $('#arrow-down-toAbout').on('click', function() {
-    event.preventDefault();
-    var targetOffset = $('#about').offset().top - 70;
-    $("html, body").animate({scrollTop: targetOffset}, 500);
-  });
+//   $('#arrow-down-toAbout').on('click', function() {
+//     event.preventDefault();
+//     var targetOffset = $('#about').offset().top - 70;
+//     $("html, body").animate({scrollTop: targetOffset}, 500);
+//   });
 
-  $('#button-toContact').on('click', function() {
-    event.preventDefault();
-    var targetOffset = $('#contact').offset().top - 70;
-    $("html, body").animate({scrollTop: targetOffset}, 500);
-  });
-});
+//   $('#button-toContact').on('click', function() {
+//     event.preventDefault();
+//     var targetOffset = $('#contact').offset().top - 70;
+//     $("html, body").animate({scrollTop: targetOffset}, 500);
+//   });
+// });
 
 // MOBILE NAVBAR MENU
 function openMenu() {
